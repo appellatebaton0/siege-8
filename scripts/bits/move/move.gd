1ecf7ce1d79b07e57d4acca63ee158119e093ffa
appellatebaton0 <145923930+appellatebaton0@users.noreply.github.com>
2025-11-18 09:31:32 -0600

    v0.1 - MoveMaster / MoveBits
    Started working on MoveBits to provide functionality to CharacterBody2Ds, through an elaborate state machine.

---
+++ scripts/bits/move/move.gd
@@ -0,0 +1,38 @@
+@abstract class_name MoveBit extends Bit
+
+@onready var mover:CharacterBody2D
+@onready var master:MoveMasterBit
+
+@onready var next_bits := get_next_bits()
+func get_next_bits() -> Array[MoveBit]:
+	var response:Array[MoveBit]
+	
+	for child in get_children(): if child is MoveBit:
+		response.append(child)
+	
+	return response
+
+func pass_call(call_name:String, arg = null):
+	if arg != null:
+		call(call_name, arg)
+		
+		for bit in next_bits: bit.pass_call(call_name, arg)
+	else:
+		call(call_name)
+		
+		for bit in next_bits: bit.pass_call(call_name)
+
+func _on_active():
+	pass
+func _on_inactive():
+	pass
+
+func _active(_delta:float):
+	pass
+func _inactive(_delta:float):
+	pass
+
+func _phys_active(_delta:float):
+	pass
+func _phys_inactive(_delta:float):
+	pass
---
+++ scripts/bits/move/move_master.gd
@@ -0,0 +1,44 @@
+class_name MoveMasterBit extends Bit
+
+@onready var mover := get_mover()
+func get_mover(depth := 2, with:Node = self) -> CharacterBody2D:
+	if depth <= 0: return null
+	if with is CharacterBody2D: return with
+	else: return get_mover(depth - 1, with.get_parent())
+
+@onready var move_bits := get_move_bits()
+func get_move_bits() -> Array[MoveBit]:
+	var response:Array[MoveBit]
+	
+	for child in get_children(): if child is MoveBit:
+		response.append(child)
+	
+	return response
+
+var current_bit:MoveBit
+@export var initial_bit:MoveBit
+
+func change_to(bit:MoveBit):
+	if current_bit != null:
+		current_bit.pass_call("_on_inactive")
+	
+	current_bit = bit
+	
+	if current_bit != null:
+		current_bit.pass_call("_on_active")
+
+func _ready() -> void:
+	if initial_bit != null:
+		change_to(initial_bit)
+	elif len(move_bits) > 0:
+		change_to(move_bits[0])
+
+func _process(delta: float) -> void:
+	for bit in move_bits:
+		if bit == current_bit: bit.pass_call("_active", delta)
+		else:                  bit.pass_call("_inactive", delta)
+
+func _physics_process(delta: float) -> void:
+	for bit in move_bits:
+		if bit == current_bit: bit.pass_call("_phys_active", delta)
+		else:                  bit.pass_call("_phys_inactive", delta)
